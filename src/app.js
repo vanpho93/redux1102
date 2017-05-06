be63d5532e15db29c070d2c8bcf43eb36dc43a0e
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-06 14:11:26 +0700

    redux

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const redux = require('redux');
+
 const App = () => (
     <div>
         <h2>App component</h2>
@@ -11,3 +13,23 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')// eslint-disable-line
 );
+
+/*
+    action la 1 object co thuoc tinh type
+    {
+        type: 'ABCD',
+        ...
+    }
+*/
+
+const reducer = (state = 0, action) => {
+    if (action.type === 'THEM') return state + 1;
+    return state;
+};
+
+const store = redux.createStore(reducer);
+
+const state = store.getState();
+console.log(state);
+store.dispatch({ type: 'THEM' });
+console.log(store.getState());
